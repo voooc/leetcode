758f0a0e58442c43561ada31e172497f3af09732
voooc <80082486+voooc@users.noreply.github.com>
2022-05-29 20:50:50 +0800

    Create 003-无重复字符的最长字串.js

---
+++ 003-无重复字符的最长字串.js
@@ -0,0 +1,25 @@
+/*
+题目：给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+输入: s = "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let set = new Set();
+  let r = -1;res =0;
+  for(let i=0;i<s.length;i++){
+    if(i != 0){
+      set.delete(s[i-1]);
+    }
+    while(r+1<s.length && !set.has(s[r+1])){
+      set.add(s[r+1]);
+      r++;
+    }
+    res = Math.max(res, r-i+1);
+  }
+  return res
+};
